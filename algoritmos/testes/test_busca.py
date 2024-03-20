a82860234b976b551e3a9e62d2af269c0c6646e5
= <=>
2024-03-20 18:49:45 -0300

    estrutura basica

---
+++ algoritmos/testes/test_busca.py
@@ -0,0 +1,4 @@
+#Busca por um elemento que está presente no vetor.
+#Busca por um elemento que não está presente no vetor.
+#Busca em um vetor vazio.
+#Busca em um vetor com um único elemento.
---
+++ algoritmos/testes/test_ordenacao.py
@@ -0,0 +1,19 @@
+
+#vetor ordenado crescente: [3, 7, 33, 59, 71]
+vetor = [3, 7, 33, 59, 71]
+#vetor não ordenado: [71, 7, 3, 9, 7]
+vetor2 = [71, 7, 3, 9, 7]
+#vetor ordenado descrescente: [71, 59, 33, 7, 3]
+vetor3 = [71, 59, 33, 7, 3]
+#Vetor vazio: []
+vetor4 = []
+#Vetor com um único elemento: [42]
+vetor5 = [42]
+#Vetor com elementos repetidos: [3, 7, 3, 9, 7]
+vetor6 = [3, 7, 3, 9, 7]
+#Vetor com elementos negativos: [-5, -3, -9, -1]
+vetor7 = [-5, -3, -9, -1]
+
+class Test_Ordenacao:
+    def __init__(self, vetor):
+        self.vetor = vetor
