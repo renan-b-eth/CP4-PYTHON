4532791675a6905becbce0bcf21032d0c1f9a60a
= <=>
2024-03-21 09:55:31 -0300

    criacao do dicionario com os dados e com o tempo de execucao dos metodos testes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import time
 import random
 import matplotlib.pyplot as plt
+#import Ordernacao 
+
+#ord = Ordernacao() # instanciar o objeto
 
 #Metodo para geracao de uma lista
 
@@ -13,5 +16,39 @@ def gerar_dados_testes(lista, tamanho_dados, inicio):
 
 lista2 = []
 
-print(gerar_dados_testes(lista2, 100, 1))
+lista3 = gerar_dados_testes(lista2, 10000, 1)
+
+print(lista3)
+
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j] ## troca de posição
+    return lista
+
+
+#metodo para definir tempo_Execucao
+def medir_tempo_execucao(algoritmo, lista):
+    inicio = time.time() ### define o tempo inicial
+    algoritmo(lista)     ### algoritmo sendo executado
+    fim = time.time()    ### tempo final
+    return fim - inicio  ### calcula o tempo total de execução
+
+tempo_bubbler = medir_tempo_execucao(bubble_sort, lista3)
+tempo_selection_sort = 1
+tempo_insertion_sort = 2
+tempo_mergeSort = 3
+tempo_quickSort = 5 
+
+#criacao do dicionario com os dados
+tempos = {'bubble_sort': tempo_bubbler,
+          'selection_sort': tempo_selection_sort,
+          'insertion_sort': tempo_insertion_sort,
+          'mergeSort': tempo_mergeSort,
+          'quickSort': tempo_quickSort}
+
+print(tempos["bubble_sort"])
+
         
