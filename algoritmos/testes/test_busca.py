71c0b3f1fd52919890619e4800af8bfe00a391f0
renan-b-eth <renanacademic21@gmail.com>
2024-04-01 21:34:59 -0300

    outros testes de buscas feito

--- algoritmos/testes/test_busca.py
+++ algoritmos/testes/test_busca.py
@@ -10,7 +10,8 @@ class Teste_Busca:
 
 
     test_ordenacao = test_ordenacao.Test_Ordenacao
-
+    #tamanho da lista
+    tamanho_lista = 1000
       
     def gerar_dados_testes(lista, tamanho_dados, inicio):
         lista = []
@@ -20,7 +21,7 @@ class Teste_Busca:
         return lista
 
     lista =  []
-    lista2 = gerar_dados_testes(lista,1000,1)
+    lista2 = gerar_dados_testes(lista,tamanho_lista,1)
 
 
     def buscar_elemento(lista, elemento):
@@ -30,26 +31,51 @@ class Teste_Busca:
                 posicoes.append(lista.index(elemento))
         return posicoes
     
-    def buscar_elemento_nao_vetor(self):
-        return print("oi")
+    def buscar_elemento_nao_vetor(lista, elemento):
+        if elemento not in lista:
+            print(str(elemento) + " nao esta na lista")
+        else:
+            print(str(elemento) + " esta na lista")
+        return ""
     
-    def buscar_elemento_vetor_vazio(self):
-        return print("oi")
+    def buscar_elemento_vetor_vazio(lista):
+        if not lista:
+            print("A lista esta vazia")
+        else:
+            print("A lista nao esta vazia")
+        return ""
     
-    def buscar_elemento_vetor_unico(self):
-        return print("oi")
+    def buscar_elemento_vetor_unico(vetor, elemento):
+        if len(vetor) == 1:
+            if vetor[0] == elemento:
+                return True
+            else:
+                return False
+        else:
+            return "O vetor nao contém um único elemento."
 
     #pode alterar o final do vetor para até o 7
-    posicao_vetores = buscar_elemento(lista2, test_ordenacao.vetor5)
+    posicao_vetores = buscar_elemento(lista2, test_ordenacao.vetor)
 
 
 
-    print("OS VALORES DO VETOR: " + str(test_ordenacao.vetor5) + " SE ENCONTRAM NOS SEGUINTES INDEX DA LISTA GERADA: " + str(posicao_vetores))
+    print("OS VALORES DO VETOR: " + str(test_ordenacao.vetor) + " SE ENCONTRAM NOS SEGUINTES INDEX DA LISTA GERADA: " + str(posicao_vetores))
+    print("")
     i=0
     while(i<len(posicao_vetores)):
         #aqui mostra os numeros encontrados nas seguintes posições
         print("POSICAO INDEX " + str(posicao_vetores[i]) + " FOI ENCONTRADO O SEGUINTE VALOR:  " + str(lista2[posicao_vetores[i]]))
         i = i + 1
+    print("")
+    elemento = 55564654   # numero exorbitante para procurar um numero na lista para dar erro
+    print(buscar_elemento_nao_vetor(lista2, elemento))
 
+    #colocar um valor depois 
+    lista3 = test_ordenacao.vetor4 #vetor4 é o vazio.
+    print(buscar_elemento_vetor_vazio(lista3))
 
+    vetor_unico = test_ordenacao.vetor5 # aqui usa o vetor5
+    elemento_busca = 5 # aqui o numero certo é 42
+    
+    print(buscar_elemento_vetor_unico(vetor_unico, elemento_busca))
     
